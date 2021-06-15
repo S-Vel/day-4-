511a8ad6e69de8c34106d572a1b24ff3f4969813
S.Vel <85781428+S-Vel@users.noreply.github.com>
2021-06-15 13:22:32 +0530

    Add files via upload

---
+++ Question 01.py
@@ -0,0 +1,15 @@
+#Day 4 task.
+#Q1.
+
+#Assigning value
+a=b=c=1000
+print("Values of a=",a,"b=",b,"and c=",c)
+
+#a divide by 10
+print("Divide a by 10 ",a/10)
+
+#b multiply by 50
+print("Multiply b by 50",b*50)
+
+#Add c value by 60
+print("Add c value by 60",c+60)
